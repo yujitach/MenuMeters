63652de13a8b507cb1628ec8f9823444b1e6c168
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2017-04-02 18:51:44 +0200

    Use Info.plist preprocessor to fill in version

---
+++ PrefPane/InfoPlistPreprocessor.h
@@ -0,0 +1,15 @@
+//
+//  InfoPlistPreprocessor.h
+//  MenuMeters
+//
+//  Created by Enrico Maria Crisostomo on 02/04/2017.
+//
+//
+
+#ifndef InfoPlistPreprocessor_h
+#define InfoPlistPreprocessor_h
+
+#define MM_VERSION 1.9.4
+#define MM_COPYRIGHT MenuMeters (El Capitan Port) v. MM_VERSION, 2015-2017 (C) Y. Tachikawa
+
+#endif /* InfoPlistPreprocessor_h */
