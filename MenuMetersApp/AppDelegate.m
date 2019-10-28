106f74a268c664babfe5f2b343b0803b0dbec51a
Yuji Tachikawa <yuji.tachikawa@ipmu.jp>
2019-10-28 16:41:31 +0900

    Basic app translocation check & Sparkle updater

--- MenuMetersApp/AppDelegate.m
+++ MenuMetersApp/AppDelegate.m
@@ -12,6 +12,9 @@
 #import "MenuMeterMemExtra.h"
 #import "MenuMeterNetExtra.h"
 #import "MenuMetersPref.h"
+#ifdef OUTOFPREFPANE
+#import <Sparkle/Sparkle.h>
+#endif
 
 @interface AppDelegate ()
 
@@ -26,6 +29,7 @@
     MenuMeterMemExtra*memExtra;
 #ifdef OUTOFPREFPANE
     MenuMetersPref*pref;
+    SUUpdater*updater;
 #endif
     NSTimer*timer;
 }
@@ -39,8 +43,14 @@
     netExtra=[[MenuMeterNetExtra alloc] initWithBundle:[NSBundle mainBundle]];
 
     memExtra=[[MenuMeterMemExtra alloc] initWithBundle:[NSBundle mainBundle]];
+    
 #ifdef OUTOFPREFPANE
     pref=[[MenuMetersPref alloc] init];
+    if([self isRunningOnReadOnlyVolume]){
+        [self alertConcerningAppTranslocation];
+    }
+    updater=[SUUpdater sharedUpdater];
+    updater.feedURL=[NSURL URLWithString:@"https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/MenuMeters-Update.xml"];
 #endif
 }
 
@@ -51,10 +61,35 @@
 
 
 #ifdef OUTOFPREFPANE
+
 - (BOOL)applicationShouldHandleReopen:(NSApplication *)sender hasVisibleWindows:(BOOL)flag
 {
     [pref.window makeKeyAndOrderFront:sender];
     return YES;
 }
+
+
+- (BOOL)isRunningOnReadOnlyVolume {
+    // taken from https://github.com/Squirrel/Squirrel.Mac/pull/186/files
+    struct statfs statfsInfo;
+    NSURL *bundleURL = NSRunningApplication.currentApplication.bundleURL;
+    int result = statfs(bundleURL.fileSystemRepresentation, &statfsInfo);
+    if (result == 0) {
+        return (statfsInfo.f_flags & MNT_RDONLY) != 0;
+    } else {
+        // If we can't even check if the volume is read-only, assume it is.
+        return YES;
+    }
+}
+
+-(void)alertConcerningAppTranslocation{
+    NSAlert*alert=[[NSAlert alloc] init];
+    alert.messageText=@"Please move the app after downloading it";
+    [alert addButtonWithTitle:@"OK, I quit the app and move it"];
+    alert.informativeText=@"Please move the app to, say, /Applications, using your mouse/trackpad, not from the command line. \n\nApple decided that they don't allow the app to auto-update otherwise. \n\nI am sorry for the inconvenience.";
+    [alert runModal];
+    [NSApp terminate:nil];
+}
+
 #endif
 @end
