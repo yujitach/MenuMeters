yujitach/MenuMeters

commit 385c074a9e01712c212443e3c9918233b4403d41

added back some indentation levels

MenuExtras/MenuMeterMem/MenuMeterMemExtra.m
@@ -121,6 +121,7 @@
 										   keyEquivalent:@""];
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 
 	// Add memory page stats title and placeholders
@@ -129,10 +130,13 @@
 										   keyEquivalent:@""];
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 
 	// Add VM stats menu items and placeholders
@@ -141,10 +145,13 @@
 										   keyEquivalent:@""];
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 
         // add items for memory pressure
@@ -153,6 +160,7 @@
                                            keyEquivalent:@""];
         [menuItem setEnabled:NO];
         menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+	    menuItem.indentationLevel=1;
         [menuItem setEnabled:NO];
 
 	// Swap file stats menu item and placeholders
@@ -161,10 +169,13 @@
 										   keyEquivalent:@""];
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 	menuItem = (NSMenuItem *)[extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
     [extraMenu addItem:[NSMenuItem separatorItem]];
     [self addStandardMenuEntriesTo:extraMenu];
@@ -178,7 +189,8 @@
 	[tempFormat setFormat:[NSString stringWithFormat:@"#,##0.0%@", [localizedStrings objectForKey:kMBLabel]]];
 	// Go through an archive/unarchive cycle to work around a bug on pre-10.2.2 systems
 	// see http://cocoa.mamasam.com/COCOADEV/2001/12/2/21029.php
-	memFloatMBFormatter = [NSUnarchiver unarchiveObjectWithData:[NSArchiver archivedDataWithRootObject:tempFormat]];
+    // memFloatMBFormatter = [NSUnarchiver unarchiveObjectWithData:[NSArchiver archivedDataWithRootObject:tempFormat]];
+    memFloatMBFormatter = tempFormat;
 	[tempFormat setFormat:[NSString stringWithFormat:@"#,##0%@", [localizedStrings objectForKey:kMBLabel]]];
 	memIntMBFormatter = [NSUnarchiver unarchiveObjectWithData:[NSArchiver archivedDataWithRootObject:tempFormat]];
 	[tempFormat setFormat:@"#,##0"];

MenuExtras/MenuMeterCPU/MenuMeterCPUExtra.m
@@ -146,6 +146,7 @@
 										   keyEquivalent:@""];
 	[menuItem setEnabled:NO];
 	menuItem = [extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 
 	// Add task title and blanks for task display
@@ -154,6 +155,7 @@
 										   keyEquivalent:@""];
 	[menuItem setEnabled:NO];
 	menuItem = [extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 
 	// Add load title and blanks for load display
@@ -162,6 +164,7 @@
 										   keyEquivalent:@""];
 	[menuItem setEnabled:NO];
 	menuItem = [extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 
 	menuItem = [extraMenu addItemWithTitle:[bundle localizedStringForKey:kCPUPowerLimitStatusTitle value:nil table:nil]
@@ -169,6 +172,7 @@
 										   keyEquivalent:@""];
 	[menuItem setEnabled:NO];
 	menuItem = [extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+    menuItem.indentationLevel=1;
 	[menuItem setEnabled:NO];
 
 	// Add top kCPUrocessCountMax most CPU intensive processes
@@ -180,6 +184,7 @@
 	// as this list is "static" unfortunately we need all of the kCPUrocessCountMax menu items and hide/show later the un-wanted/wanted ones
 	for (NSInteger ndx = 0; ndx < kCPUrocessCountMax; ++ndx) {
 		menuItem = [extraMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
+        menuItem.indentationLevel=1;
 		[menuItem setEnabled:NO];
 	}
 

commit e1df58bd213c9062a37050ac65b555942c32480c

fixes a bug introduced while refactoring
in 385c074a9e01712c212443e3c9918233b4403d41 .  Ugh.

MenuExtras/MenuMeterMem/MenuMeterMemExtra.m
@@ -189,8 +189,7 @@
 	[tempFormat setFormat:[NSString stringWithFormat:@"#,##0.0%@", [localizedStrings objectForKey:kMBLabel]]];
 	// Go through an archive/unarchive cycle to work around a bug on pre-10.2.2 systems
 	// see http://cocoa.mamasam.com/COCOADEV/2001/12/2/21029.php
-    // memFloatMBFormatter = [NSUnarchiver unarchiveObjectWithData:[NSArchiver archivedDataWithRootObject:tempFormat]];
-    memFloatMBFormatter = tempFormat;
+    memFloatMBFormatter = [NSUnarchiver unarchiveObjectWithData:[NSArchiver archivedDataWithRootObject:tempFormat]];
 	[tempFormat setFormat:[NSString stringWithFormat:@"#,##0%@", [localizedStrings objectForKey:kMBLabel]]];
 	memIntMBFormatter = [NSUnarchiver unarchiveObjectWithData:[NSArchiver archivedDataWithRootObject:tempFormat]];
 	[tempFormat setFormat:@"#,##0"];